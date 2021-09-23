6cec8df1c8c2024f6526523632f01c8f95172bb9
running fd app <nkadu34@gmail.com>
2021-09-23 12:08:52 +0530

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog_app/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Material(
-          child: Container(
-            child: Text('Welocme to flutter'),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
